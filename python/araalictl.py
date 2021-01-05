4ba581c704e44dce76b1ca612cf46d0f94877592
Abhishek Singh <abhishek@araalinetworks.com>
2021-01-05 11:53:44 -0800

    upgrade support
    python araalictl.py to download, see README.md for usage

--- python/araalictl.py
+++ python/araalictl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import requests
+import sys
 from sys import platform
 
 from main import Usage, run_command
@@ -15,6 +16,7 @@ def fetch():
         url = darwin_url
     else:
         raise Usage("only linux and mac are currently supported")
+    print("Fetching araalictl ...")
     r = requests.get(url, allow_redirects=True)
     open('araalictl', 'wb').write(r.content)
     os.chmod('araalictl', 0o777)
@@ -23,3 +25,5 @@ def fetch():
 def help():
     print(run_command("./araalictl -h", result=True, debug=False)[1])
 
+if __name__ == '__main__':
+    sys.exit(fetch())
