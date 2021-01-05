0fb828a54d5748073230877cb4a0748929bee12d
Abhishek Singh <abhishek@araalinetworks.com>
2021-01-05 11:32:31 -0800

    basic integration with araalictl

---
+++ python/araalictl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import requests
+from sys import platform
+
+from main import Usage, run_command
+
+def fetch():
+    linux_url = "https://s3-us-west-2.amazonaws.com/araalinetworks.cf/araalictl.linux-amd64"
+    darwin_url = "https://s3-us-west-2.amazonaws.com/araalinetworks.cf/araalictl.darwin-amd64"
+
+    if platform == "linux" or platform == "linux2":
+        url = linux_url
+    elif platform == "darwin":
+        url = darwin_url
+    else:
+        raise Usage("only linux and mac are currently supported")
+    r = requests.get(url, allow_redirects=True)
+    open('araalictl', 'wb').write(r.content)
+    os.chmod('araalictl', 0o777)
+
+
+def help():
+    print(run_command("./araalictl -h", result=True, debug=False)[1])
+
