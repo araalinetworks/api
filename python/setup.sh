71dca7bd898846e52f7184d4b395a97537a99315
Abhishek Singh <abhishek@araalinetworks.com>
2022-07-03 13:44:32 -0700

    change the env name

--- python/setup.sh
+++ python/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3 -m venv araali
-source araali/bin/activate && python3 -m pip install --upgrade pip
-source araali/bin/activate && pip install -r requirements.txt
+python3 -m venv araalienv
+source araalienv/bin/activate && python3 -m pip install --upgrade pip
+source araalienv/bin/activate && pip install -r requirements.txt
 python araalictl.py
 sudo ./araalictl authorize -local
-source araali/bin/activate && bash
+source araalienv/bin/activate && bash
